8e775a7b89792f94b15e0858f2a48e53f962217f
Matheus Henrique <teus.henrique021@gmail.com>
2026-02-11 19:47:54 -0300

    Menu interativo

--- Main.java
+++ Main.java
@@ -5,7 +5,17 @@ public class Main {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bem-vindo a CALCULADORA");
+        System.out.println("\nBem-vindo a CALCULADORA\n");
 
+        while (true){
+            System.out.println("---MENU---");
+            System.out.println("1 - Adição");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Multiplicação");
+            System.out.println("4 - Divisão\n");
+
+            System.out.print("Digite o índice da opção que deseja utilizar: ");
+            int opcao = sc.nextInt();
+        }
     }
 }
