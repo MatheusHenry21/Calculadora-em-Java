deec3429a3b3182635ae605478c589a9349ef60a
Matheus Henrique <teus.henrique021@gmail.com>
2026-02-11 19:32:21 -0300

    Estrutura base do Java

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem-vindo a CALCULADORA");
+
+    }
+}
